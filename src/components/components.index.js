a8ceca6db03b4a4a37e784347ea6456545da9af6
Gittenger <36045366+Gittenger@users.noreply.github.com>
2022-01-07 01:09:15 -0500

    hamburger menu working

--- src/components/components.index.js
+++ src/components/components.index.js
@@ -1,8 +1,9 @@
+import HamburgerMenu from "./General/HamburgerMenu.component.jsx"
 import Header from "./General/Header.component.jsx"
 import Layout from "./General/Layout.component.jsx"
 import Login from "./General/Login.component.jsx"
 export const CIndex = {
-		Header,Layout,Login
+		HamburgerMenu,Header,Layout,Login
 	}
 	export default CIndex
 	
