acb81766893fba974dbf1e486e871348d3fc4514
Gittenger <36045366+Gittenger@users.noreply.github.com>
2022-01-08 17:30:21 -0500

    rm unnec comp

--- src/components/components.index.js
+++ src/components/components.index.js
@@ -1,9 +1,7 @@
 import HamburgerMenu from './General/HamburgerMenu.component.jsx'
-import Header from './General/Header.component.jsx'
 import Layout from './General/Layout.component.jsx'
 export const CIndex = {
   HamburgerMenu,
-  Header,
   Layout,
 }
 export default CIndex
