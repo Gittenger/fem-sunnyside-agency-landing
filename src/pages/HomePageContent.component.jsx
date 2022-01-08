1941e44823842f6a4078846b953d12e6877e4949
Gittenger <36045366+Gittenger@users.noreply.github.com>
2022-01-08 17:31:18 -0500

    top of header mobile good

--- src/pages/HomePageContent.component.jsx
+++ src/pages/HomePageContent.component.jsx
@@ -1,6 +1,14 @@
 import React, { useEffect } from 'react'
 
 import CIndex from '../components/components.index.js'
+import HeaderImg from '../assets/img/mobile/image-header.jpg'
+import logoImg from '../assets/icons/logo.svg'
+
+const arbStyles = {
+  logo: {
+    top: '3.3rem',
+  },
+}
 
 const HomePageContent = () => {
   const { HamburgerMenu } = CIndex
@@ -11,7 +19,17 @@ const HomePageContent = () => {
 
   return (
     <>
-      <div className="w-full grid justify-items-center px-5">
+      <div className="w-full grid justify-items-center">
+        <div className="w-full">
+          <img src={HeaderImg} alt="" />
+        </div>
+        <img
+          src={logoImg}
+          alt=""
+          className="absolute top-14 left-8"
+          style={arbStyles.logo}
+        />
+
         <HamburgerMenu />
       </div>
     </>
