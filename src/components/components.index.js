a4510c06f936ad4f9304cde58b72493ce228744c
Gittenger <36045366+Gittenger@users.noreply.github.com>
2022-01-08 16:09:28 -0500

    rm unnecessary comp

--- src/components/components.index.js
+++ src/components/components.index.js
@@ -1,9 +1,9 @@
-import HamburgerMenu from "./General/HamburgerMenu.component.jsx"
-import Header from "./General/Header.component.jsx"
-import Layout from "./General/Layout.component.jsx"
-import Login from "./General/Login.component.jsx"
+import HamburgerMenu from './General/HamburgerMenu.component.jsx'
+import Header from './General/Header.component.jsx'
+import Layout from './General/Layout.component.jsx'
 export const CIndex = {
-		HamburgerMenu,Header,Layout,Login
-	}
-	export default CIndex
-	
+  HamburgerMenu,
+  Header,
+  Layout,
+}
+export default CIndex
