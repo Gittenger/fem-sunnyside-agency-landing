d3671c10a47e87ae25bd7e739ca5dd18c3c6766b
Gittenger <36045366+Gittenger@users.noreply.github.com>
2022-01-10 14:03:37 -0500

    updated home page content for features

--- src/pages/HomePageContent.component.jsx
+++ src/pages/HomePageContent.component.jsx
@@ -1,17 +1,19 @@
 import React, { useEffect } from 'react'
 
 import CIndex from '../components/components.index.js'
-import HeaderImg from '../assets/img/mobile/image-header.jpg'
-import logoImg from '../assets/icons/logo.svg'
 
 const arbStyles = {
   logo: {
     top: '3.3rem',
   },
+  headerText: {
+    color: '#fffff3',
+    fontSize: '2.6rem',
+  },
 }
 
 const HomePageContent = () => {
-  const { HamburgerMenu } = CIndex
+  const { Header, Transform } = CIndex
 
   useEffect(() => {
     window.scrollTo(0, 0)
@@ -20,17 +22,9 @@ const HomePageContent = () => {
   return (
     <>
       <div className="w-full grid justify-items-center">
-        <div className="w-full">
-          <img src={HeaderImg} alt="" />
-        </div>
-        <img
-          src={logoImg}
-          alt=""
-          className="absolute top-14 left-8"
-          style={arbStyles.logo}
-        />
+        <Header />
 
-        <HamburgerMenu />
+        <Transform />
       </div>
     </>
   )
