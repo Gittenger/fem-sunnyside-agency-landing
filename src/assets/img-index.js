d8813353414e495322d35cab7449f312844cea35
Gittenger <36045366+Gittenger@users.noreply.github.com>
2022-01-10 14:00:19 -0500

    updated imgs

--- src/assets/img-index.js
+++ src/assets/img-index.js
@@ -1,6 +1,19 @@
+import headerMobile from './img/mobile/image-header.jpg'
+import transformMob from './img/mobile/image-transform.jpg'
+import arrowDown from './icons/icon-arrow-down.svg'
+import logoImg from './icons/logo.svg'
+
 export const images = {
-  img: {},
-  svg: {},
+  img: {
+    mobile: {
+      headerMobile,
+      transformMob,
+    },
+  },
+  svg: {
+    arrowDown,
+    logoImg,
+  },
   tiny: {},
 }
 
