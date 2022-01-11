7dbde79c3de399d7595f38931ac2c2d3187ae30f
Gittenger <36045366+Gittenger@users.noreply.github.com>
2022-01-11 01:58:40 -0500

    active states for btns

--- src/components/General/BtnUline.component.jsx
+++ src/components/General/BtnUline.component.jsx
@@ -3,20 +3,16 @@ import styles from './styles/BtnUline.module.css'
 
 const BtnUline = ({ color }) => (
   <button
-    className={`mt-6 mb-10 font-heading uppercase ${styles.parent} ${
-      color === 'yellow' ? 'yellow' : color === 'red' ? 'red' : 'blue'
+    className={`mt-6 mb-10 font-heading uppercase ${styles.btnUline} ${
+      color === 'yellow'
+        ? styles.yellow
+        : color === 'red'
+        ? styles.red
+        : styles.colorDefault
     }`}
   >
     <div className="tracking-wide">Learn More</div>
-    <div
-      className={`h-1.5 w-full -mt-1.5 ${
-        color === 'yellow'
-          ? styles.yellow
-          : color === 'red'
-          ? styles.red
-          : styles.colorDefault
-      }`}
-    ></div>
+    <div className={`h-1.5 w-full -mt-1.5`}></div>
   </button>
 )
 
