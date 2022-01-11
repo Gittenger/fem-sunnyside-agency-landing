e72d634c265fcf15d87701db313e2ecfb4a48a10
Gittenger <36045366+Gittenger@users.noreply.github.com>
2022-01-11 01:27:03 -0500

    photo/graphic looks good thru sm styles

--- src/components/Features/Graphic.component.jsx
+++ src/components/Features/Graphic.component.jsx
@@ -1,29 +1,19 @@
 import React from 'react'
-import images from '../../assets/img-index.js'
 import CIndex from '../components.index.js'
-
-const arbStyles = {
-  bg: {
-    backgroundImage: `url(${images.img.mobile.graphicMob})`,
-    backgroundPosition: 'top 0 center',
-    backgroundSize: 'cover',
-    height: '42rem',
-  },
-}
+import styles from './styles/Graphic.module.css'
 
 const Graphic = () => {
   const {} = CIndex
 
   return (
     <div
-      style={arbStyles.bg}
-      className="relative w-full flex flex-col justify-center items-center text-blackGr"
+      className={`relative w-full flex flex-col justify-center items-center text-blackGr ${styles.bg} ${styles.bgGraphic}`}
     >
-      <div className="absolute bottom-16 flex-flex-col justify-center items-center">
+      <div className="absolute bottom-16 sm:bottom-32 flex-flex-col justify-center items-center">
         <h2 className="font-bold text-3xl font-heading text-center">
           Graphic Design
         </h2>
-        <p className="mt-8 px-8 font-body text-center">
+        <p className="mt-8 px-8 xs:px-16 sm:px-28 font-body text-center">
           Great design makes you memorable. We deliver artwork that underscores
           your brand message and captures potential clients’ attention.
         </p>
--- src/components/Features/Photography.component.jsx
+++ src/components/Features/Photography.component.jsx
@@ -1,29 +1,20 @@
 import React from 'react'
 import images from '../../assets/img-index.js'
 import CIndex from '../components.index.js'
-
-const arbStyles = {
-  bg: {
-    backgroundImage: `url(${images.img.mobile.photoMob})`,
-    backgroundPosition: 'top 0 center',
-    backgroundSize: 'cover',
-    height: '42rem',
-  },
-}
+import styles from './styles/Graphic.module.css'
 
 const Photography = () => {
   const {} = CIndex
 
   return (
     <div
-      style={arbStyles.bg}
-      className="relative w-full flex flex-col justify-center items-center text-blackBl"
+      className={`relative w-full flex flex-col justify-center items-center text-blackBl ${styles.bg} ${styles.bgPhoto}`}
     >
-      <div className="absolute bottom-16 flex-flex-col justify-center items-center">
+      <div className="absolute bottom-16 sm:bottom-32 flex-flex-col justify-center items-center">
         <h2 className="font-bold text-3xl font-heading text-center">
           Photography
         </h2>
-        <p className="mt-8 px-8 font-body text-center">
+        <p className="mt-8 px-8 xs:px-16 sm:px-28 font-body text-center">
           Increase your credibility by getting the most stunning, high-quality
           photos that improve your business image.
         </p>
