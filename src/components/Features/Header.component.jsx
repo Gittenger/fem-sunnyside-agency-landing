108800b4b651c1a562b0c8da3bb9d253c7f543a8
Gittenger <36045366+Gittenger@users.noreply.github.com>
2022-01-11 01:41:10 -0500

    mq for header

--- src/components/Features/Header.component.jsx
+++ src/components/Features/Header.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import CIndex from '../components.index.js'
 import images from '../../assets/img-index.js'
+import styles from './styles/Header.module.css'
 
 const arbStyles = {
   logo: {
@@ -35,9 +36,9 @@ const Header = () => {
 
       <HamburgerMenu className="z-50" />
 
-      <div className="absolute grid place-content-center place-items-center pt-36">
+      <div className="absolute grid place-content-center place-items-center pt-36 xs:pt-52 sm:pt-64 w-full">
         <h1
-          className="text-5xl uppercase tracking-widest leading-snug font-heading font-black text-center"
+          className="text-5xl uppercase tracking-widest leading-snug font-heading font-black text-center xs:px-14 sm:px-32"
           style={arbStyles.headerText}
         >
           We are Creatives
