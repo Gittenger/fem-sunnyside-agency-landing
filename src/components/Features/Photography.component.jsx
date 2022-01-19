5c2029a927fce38ab8bb98a256d1a27c48ca2279
Gittenger <36045366+Gittenger@users.noreply.github.com>
2022-01-19 14:24:06 -0500

    Transform/Photography implement BgImgCard

--- src/components/Features/Photography.component.jsx
+++ src/components/Features/Photography.component.jsx
@@ -1,21 +1,17 @@
 import React from 'react'
-import styles from './styles/Graphic.module.css'
+import styles from './styles/Photo.module.css'
+import CIndex from '../components.index'
 
 const Photography = () => {
+  const { BgImgCard } = CIndex
+
   return (
-    <div
-      className={`relative w-full flex flex-col justify-center items-center text-blackBl ${styles.bg} ${styles.bgPhoto}`}
-    >
-      <div className="absolute bottom-16 sm:bottom-32 flex-flex-col justify-center items-center">
-        <h2 className="font-bold text-3xl font-heading text-center">
-          Photography
-        </h2>
-        <p className="mt-8 px-8 xs:px-16 sm:px-28 font-body text-center">
-          Increase your credibility by getting the most stunning, high-quality
-          photos that improve your business image.
-        </p>
-      </div>
-    </div>
+    <BgImgCard
+      bgImgClass={styles.bgImg}
+      title="Photography"
+      text="Increase your credibility by getting the most stunning, high-quality photos that improve your business image."
+      color="blue"
+    />
   )
 }
 
--- src/components/Features/Transform.component.jsx
+++ src/components/Features/Transform.component.jsx
@@ -6,6 +6,11 @@ const arbStyles = {
   container: {
     backgroundColor: '#fffbf8',
   },
+  img: {
+    objectFit: 'stretch',
+    height: '100%',
+    width: '100%',
+  },
 }
 
 const Transform = () => {
@@ -13,24 +18,30 @@ const Transform = () => {
   const {
     img: {
       mobile: { transformMob },
+      desktop: { transformDesk },
     },
   } = images
 
   return (
     <div
       style={arbStyles.container}
-      className="text-black flex flex-col justify-center items-center"
+      className="text-black grid grid-cols-1 grid-rows-2 justify-center items-center  sm2:grid-cols-2 sm2:grid-rows-1"
     >
-      <img src={transformMob} alt="" />
-      <h2 className="mt-16 text-3xl font-heading font-bold text-center">
-        Transform your <br /> brand
-      </h2>
-      <p className="mt-6 text-center px-8 font-body">
-        We are a full-service creative agency specializing in helping brands
-        grow fast. Engage your clients through compelling visuals that do most
-        of the marketing for you.
-      </p>
-      <BtnUline color="yellow" />
+      <picture className="w-full h-full sm2:col-start-2 sm2:col-end-3">
+        <source srcSet={transformDesk} media="(min-width:750px)" />
+        <img src={transformMob} alt="" style={arbStyles.img} />
+      </picture>
+      <div className="flex flex-col justify-center items-center sm2:col-start-1 col-end-2 sm2:row-start-1 sm2:row-end-2">
+        <h2 className="mt-16 text-3xl font-heading font-bold text-center">
+          Transform your <br /> brand
+        </h2>
+        <p className="mt-6 text-center px-8 font-body">
+          We are a full-service creative agency specializing in helping brands
+          grow fast. Engage your clients through compelling visuals that do most
+          of the marketing for you.
+        </p>
+        <BtnUline color="yellow" />
+      </div>
     </div>
   )
 }
