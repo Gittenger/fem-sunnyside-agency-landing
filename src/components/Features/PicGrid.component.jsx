8ab4ab017307bce9def4b5d6be0acba6eb03028b
Gittenger <36045366+Gittenger@users.noreply.github.com>
2022-01-20 13:11:26 -0500

    updated picgrid for srcSet

--- src/components/Features/PicGrid.component.jsx
+++ src/components/Features/PicGrid.component.jsx
@@ -14,10 +14,22 @@ const PicGrid = () => {
     <div
       className={`grid grid-cols-2 grid-rows-2 sm2:grid-cols-4 sm2:grid-rows-1 w-full ${styles.grid}`}
     >
-      <img src={milkMobile} alt="" />
-      <img src={orangeMobile} alt="" />
-      <img src={coneMobile} alt="" />
-      <img src={sugarMobile} alt="" />
+      <picture>
+        <source srcSet={milkDesk} media="(min-width: 750px)" />
+        <img src={milkMobile} alt="" />
+      </picture>
+      <picture>
+        <source srcSet={orangeDesk} media="(min-width: 750px)" />
+        <img src={orangeMobile} alt="" />
+      </picture>
+      <picture>
+        <source srcSet={coneDesk} media="(min-width: 750px)" />
+        <img src={coneMobile} alt="" />
+      </picture>
+      <picture>
+        <source srcSet={sugarDesk} media="(min-width: 750px)" />
+        <img src={sugarMobile} alt="" />
+      </picture>
     </div>
   )
 }
